07178dcf2de290959b352735739846564ea238be
itmao <1280337559@qq.com>
2022-11-03 16:02:46 +0800

    测试

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
+//导入body-parser 解析器中间件
+const bodyParser = require('body-parser');
+// 解析 application/x-www-form-urlencoded 格式数据
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/api', (req, res) => {
   res.send('欢迎来到小毛接口服务')
 });
 app.get('/api/index', (req, res) => {
-  res.send('index打开了')
+  res.send('index打开了',req.query)
 })
-app.get('/api/user', (req,res)=> {
+app.post('/api/index', (req,res)=> {
   res.send({
-    username: '张三',
-    age:18
-  })
+    msg: '请求成功',
+    status: 200,
+    data:req.body
+ })
 })
 module.exports=app
