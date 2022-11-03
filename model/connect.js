5efd187ed6966734746821d9c77508bb4ab21f63
itmao <1280337559@qq.com>
2022-11-03 09:38:55 +0800

    ass

--- model/connect.js
+++ model/connect.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://itmao:123456@127.0.0.1:27017/Students', { useNewUrlParser: true })
 //     .then(()=>console.log('mongodb数据库连接成功!'))
 //     .catch((err)=>console.log('数据库连接失败！'));
-// mongoose.connect('mongodb+srv://itmao:maoyelin@cluster0.w6qcflo.mongodb.net/Students?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true})
-// .then(() => console.log('数据库连接成功'))
-// .catch(() => console.log('数据库连接失败'));
+mongoose.connect('mongodb+srv://itmao:maoyelin@cluster0.w6qcflo.mongodb.net/Students?retryWrites=true&w=majority')
+.then(() => console.log('数据库连接成功'))
+.catch(() => console.log('数据库连接失败'));
